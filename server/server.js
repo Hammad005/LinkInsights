de04730b59f120f85c6622502ff8e6e792b8d639
Hammad <127682041+Hammad005@users.noreply.github.com>
2026-06-17 01:12:36 +0500

    fix: update CORS configuration to allow all origins

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: '*',
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
